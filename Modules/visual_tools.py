d7288211a4142e955fbea54982db879ec94c9cc6
jeremy collin <jeremy.collin@univ-brest.fr>
2019-05-03 11:25:43 +0200

    Visual Tool box features

--- Modules/visual_tools.py
+++ Modules/visual_tools.py
@@ -5,14 +5,9 @@ import numpy as np
 import numpy.ma as ma
 from netCDF4 import Dataset
 
-
 import sys
 sys.path.append('../Modules/')
 from R_files import load
-
-
-
-
 
 ##############################################################################
 def get_var(var, ncfile, **kwargs):
@@ -20,19 +15,26 @@ def get_var(var, ncfile, **kwargs):
     Returns the netcdf variable 'var' from ncfile
     var is a string : name of variable
     optionnal: itime time index for slicing
+
+    keyword argument : ndims, number of dimension of variable in netcdf file
+                       ex: 4 for w(t,z,y,x)
+                       Much faster when itime is defined
     return np.array py_var
     '''
     nc = Dataset(ncfile, 'r')
     if var in nc.variables:
         if 'itime' in kwargs:
             itime = kwargs['itime']
-       	    ndims = len(nc.variables[var][:].shape)
+       	    if 'ndims' not in kwargs:
+                ndims = len(nc.variables[var][:].shape)
+            else:
+                 ndims = kwargs['ndims']
             if ndims == 3:
                 py_var = nc.variables[var][itime, :, :]
             elif ndims == 4:
                 py_var = nc.variables[var][itime, :, : ,:]
             else: 
-                print(f'Error dimension {ndim-1} is not supported')
+                print(f'Error dimension {ndims-1} is not supported')
         else:
             py_var = nc.variables[var][:]
     else:
@@ -42,7 +44,6 @@ def get_var(var, ncfile, **kwargs):
     return py_var
 
 #############################################################################
-
 def get_attr(attr, ncfile, it=0):
     '''
     Returns the netcdf attribute 'attr' from ncfile
@@ -58,8 +59,8 @@ def get_attr(attr, ncfile, it=0):
     return py_attr
 
 ##############################################################################
-def map_traj(pvar, px, py, map_var=[], ng=2, mask=[], cmap1='bone', 
-             cmap2='jet', **kwargs):
+def map_traj(pvar, px, py, map_var=[], method='pcolormesh', ng=2, mask=[],
+             cmap1='bone', cmap2='jet', **kwargs):
     '''
     Plot particles trajectory with pvar in color 
     Superimposed with another 2D variable 
@@ -73,8 +74,14 @@ def map_traj(pvar, px, py, map_var=[], ng=2, mask=[], cmap1='bone',
 
     keyword arguments:
         coord = [ymin, ymax, xmin, max] : plot axes bounds
+        vmin1, vmax1 : colorbar extent for pvar
+		vmin2, vmax2 : colorbar extent for map_var
+		title
+		xlabel
+		ylabel
 
     '''
+	# 2D plot extents
     if 'coord' in kwargs:
         coord = kwargs['coord']
         [ymin, ymax, xmin, xmax] = coord[0:4]
@@ -83,30 +90,117 @@ def map_traj(pvar, px, py, map_var=[], ng=2, mask=[], cmap1='bone',
         xmax = int(np.ceil(np.nanmax(px))) + ng
         ymin = int(np.floor(np.nanmin(py))) - ng
         ymax = int(np.ceil(np.nanmax(py))) + ng
+    # Mask lands
     if len(mask) > 0:
+	    # in case where map_var is not a masked array raise an exception ?
         map_var.mask = mask
-        map_var.mask = ~map_var.mask 
+        map_var.mask = ~map_var.mask
+	# pvar color scale	
     if ('vmin1' in kwargs) and ('vmax1' in kwargs):
         vmin1 = kwargs['vmin1']
         vmax1 = kwargs['vmax1']
     else:
         vmin1 = np.nanmin(pvar)
         vmax1 = np.nanmax(pvar)
-     
-    fig = plt.figure()
-    vmin = np.nanmin(pvar)
-    vmax = np.nanmax(pvar)
+	# map_var color scale
+    if ('vmin2' in kwargs) and ('vmax2' in kwargs):
+        vmin2 = kwargs['vmin2']
+        vmax2 = kwargs['vmax2']
+    else:
+        vmin2 = np.nanmin(map_var)
+        vmax2 = np.nanmax(map_var)
+    # Text
+    if 'title' in kwargs:
+	    title = kwargs['title']
+    else:
+        title = ''
+    if 'xlabel' in kwargs:
+        xlabel = kwargs['xlabel']
+    else:
+        xlabel = ''
+    if 'ylabel' in kwargs:
+        ylabel = kwargs['ylabel']
+    else:
+        ylabel = ''
+
+    fig = plt.figure
+    #vmin = np.nanmin(pvar)
+    #vmax = np.nanmax(pvar)
     plt.scatter(px, py, c=pvar, cmap=cmap1, alpha=0.75, vmin=vmin1,
                 vmax=vmax1, zorder=10)
-    cbar1 = plt.colorbar(orientation='horizontal', shrink=0.25, pad=0.05)
-    plt.pcolormesh(ma.masked_invalid(map_var), cmap=cmap2,
-                   rasterized=True)
+    cbar1 = plt.colorbar(orientation='horizontal', shrink=0.25, pad=0.1)
+    if method == 'pcolormesh':
+        plt.pcolormesh(ma.masked_invalid(map_var), cmap=cmap2,
+                       rasterized=True, alpha=0.5, vmin=vmin2, vmax=vmax2)
+    elif method == 'contourf':
+	    
+        plt.contourf(ma.masked_invalid(map_var), cmap=cmap2, rasterized=True,
+    	             alpha=0.5, vmin=vmin2, vmax=vmax2)
+    plt.xlabel(xlabel)
     cbar2 = plt.colorbar(shrink=0.25);
     plt.axis([xmin, xmax, ymin, ymax])
-    plt.show()
+    plt.ylabel(ylabel)
     return fig
 ##############################################################################
+def plot_traj(pvar, px, py, cmap='jet', marker='o', zorder=1,
+              **kwargs):
+    '''
+    Plot particles trajectory along (px,py) with pvar in color 
 
+    parameters : 
+        pvar variable at particles points
+        px, py: particles horizontal pozition
+        cmap : colormap for scatter plot
+        marker : string, marker shape of scatter plot 
+        zorder: float 
+		        Set the zorder for the artist.
+				Artists with lower zorder values are drawn first. 
+	
+	keyword arguments:
+        size : scalar or array_like, shape (n, ), optional
+		       The marker size in points**2.
+		       Default is rcParams['lines.markersize'] ** 2. 
+		coord = [ymin, ymax, xmin, max] : plot axes bounds
+        vmin, vmax :floats, colorbar extent for pvar
+        title 
+        xlabel
+        ylabel
+
+    '''
+    # Marker size
+    if 'size' in kwargs:
+        size = kwargs['size']
+    else:
+        size = plt.rcParams['lines.markersize'] ** 2
+    # pvar color scale  
+    if ('vmin' in kwargs) and ('vmax' in kwargs):
+        vmin = kwargs['vmin']
+        vmax = kwargs['vmax']
+    else:
+        vmin = np.nanmin(pvar)
+        vmax = np.nanmax(pvar)
+    # Text
+    if 'title' in kwargs:
+        title = kwargs['title']
+    else:
+        title = ''
+    if 'xlabel' in kwargs:
+        xlabel = kwargs['xlabel']
+    else:
+        xlabel = ''
+    if 'ylabel' in kwargs:
+        ylabel = kwargs['ylabel']
+    else:
+        ylabel = ''
+
+    #fig = plt.figure
+    plt.scatter(px, py, c=pvar, cmap=cmap, alpha=0.75, vmin=vmin,
+                vmax=vmax, zorder=10, marker=marker, s=size)
+    #cbar = plt.colorbar(orientation='horizontal', shrink=0.25, pad=0.1)
+    #plt.xlabel(xlabel)
+    #plt.ylabel(ylabel)
+    return #fig
+##############################################################################
 
 
 
