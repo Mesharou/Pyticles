95c252cfe78f256ebbe49affc42379cb3fef075b
jeremy collin <jeremy.collin@univ-brest.fr>
2023-10-02 12:36:44 +0000

    added R_tools, R_vars, R_smooth

---
+++ Modules/R_tools/lmd_wscale_ws_only.h
@@ -0,0 +1,54 @@
+! Compute turbulent velocity scales for tracers, ws=ws(zscale,Bfsfc),
+! where where zscale is distance from the surface and Bfsfc buoyancy
+! forcing.  The procedure of computation follows Eqs. (B1a)-(B1e) of
+! LMD1994 paper with constants zeta_s, a_s, c_s specified in Eq.(B2).
+! Mixing length scale "zscale" is limited by a specified fraction of
+! boundary layer thickness in the case of unstable buoyancy forcing
+! (as in the original 1994 code); or in both stable and unstable
+! forcing (modification of Gokhan and Bill Large in 2003).
+! Adapted from Bill Large 1995/2003 code.
+!
+! input variables: zscale, Bfsfc (both are volatile scalars),
+!                  ustar,  hbl (hbl is only for limiting of zscale)
+! zeta_s, a_s, c_s are constants
+!
+! output: ws (volatile scalar)
+!
+!
+# ifdef LIMIT_UNSTABLE_ONLY
+          if (Bfsfc .lt. 0.) zscale=min(zscale, hbl(i,j)*epssfc)
+# else
+          zscale=min(zscale, hbl(i,j)*epssfc)
+# endif
+
+     
+          zetahat=vonKar*zscale*Bfsfc
+          ustar3=ustar(i,j)**3
+ 
+          if ((k.eq.N-1) .and. (j.eq.1) .and. (i.eq.181))
+     &    write(*,*)'zetahat',zetahat
+              
+          
+!
+! Stable regime.
+!
+          if (zetahat .ge. 0.) then
+            ws=vonKar*ustar(i,j)*ustar3/max(ustar3+5.*zetahat,
+     &                                                 1.E-20)
+                      
+            
+!
+! Unstable regime: note that zetahat is always negative here, also
+! negative is the constant "zeta_s", hence "ustar" must be positive 
+! and bounded away from zero for this condition to be held.
+!
+          elseif (zetahat .gt. zeta_s*ustar3) then
+            ws=vonKar*( (ustar3-16.*zetahat)/ustar(i,j) )**r2
+!
+! Convective regime: note that unlike the two cases above, this
+! results in non-zero "ws" even in the case when ustar==0. 
+!
+          else
+            ws=vonKar*(a_s*ustar3-c_s*zetahat)**r3
+          endif
+                     !--> discard zetahat, ustar3
